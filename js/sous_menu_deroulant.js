901b138cc423cd4bbf08746f0a46ca53d58556ce
Sergio Gonzalez <s.gonzalez@codeur.online>
2022-05-08 19:31:33 +0200

    header et index terminé

--- js/sous_menu_deroulant.js
+++ js/sous_menu_deroulant.js
@@ -1,11 +1,33 @@
-deroulant=document.getElementById('deroulant');
-overlay=document.getElementsById('overlay');
+deroulant=document.getElementsByClassName('deroulant');
+sousMenu=document.getElementsByClassName('sous_menu')
+overlay=document.getElementById('overlay');
 
-function derouler(){
-    if (deroulant.firstElementChild.style.display == 'flex'){
-        deroulant.firstElementChild.style.display = 'none';
-    }else{
-        deroulant.firstElementChild.style.display = 'flex';
+for(let i=0; i < deroulant.length; i++){
+    deroulant[i].onclick = function(e){
+        if(e.target == deroulant[i].firstElementChild){   
+            for(let j=1; j<deroulant.length; j++){
+                sousMenu[j].style.display = 'none';
+            }
+            if (sousMenu[i].style.display == 'flex'){
+                sousMenu[i].style.display = 'none';
+                if(i==0){
+                    overlay.style.display = 'none';
+                }
+            }else{
+                sousMenu[i].style.display = 'flex';
+                if(i==0){
+                    overlay.style.display = 'block';
+                }
+            }
+        }
+    } 
+}
 
+overlay.onclick = function(){
+    overlay.style.display = 'none';
+    for(let i = 0 ; i<deroulant.length; i++){
+        sousMenu[i].style.display = 'none';
     }
 }
+    
+
