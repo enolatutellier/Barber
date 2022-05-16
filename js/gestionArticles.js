12deadf308cc254eb7a22e927adb59e8946b3da3
Enola Tutellier <enola.tutellier@hotmail.com>
2022-05-16 09:55:07 +0200

    page connexion fini ajout footer+header

--- js/gestionArticles.js
+++ js/gestionArticles.js
@@ -1,88 +1,85 @@
-function GetData()
-{
+function GetData() {
   let typeCat = document.getElementById("type_principal").value;
   $.ajax({
     type: 'post',
     url: 'getArticles.php',
     data: {
-      table:typeCat
+      table: typeCat
     },
     success: function (response) {
       $('#articles').html(response);
     }
   });
-    
+
   return false;
 }
 
-function confirmation(e){
-    resultat = window.confirm("Etes-vous sûr(e) de vouloir supprimer cet article ?");
-    if (resultat){
-        suppArticle(e);
+function confirmation(e) {
+  resultat = window.confirm("Etes-vous sûr(e) de vouloir supprimer cet article ?");
+  if (resultat) {
+    suppArticle(e);
+  }
+}
+
+function suppArticle(e) {
+  let name = e.parentElement.nextElementSibling.firstElementChild.innerHTML;
+  let typeCat = document.getElementById("type_principal").value;
+  $.ajax({
+    type: 'post',
+    url: 'supprimerArticle.php',
+    data: {
+      table: typeCat,
+      nom: name
+    },
+    success: function (response) {
+      alert('element supprimé');
+      GetData();
     }
+  });
+  return false;
 }
 
-function suppArticle(e)
-{
-    let name = e.parentElement.nextElementSibling.firstElementChild.innerHTML;
-    let typeCat = document.getElementById("type_principal").value;
-    $.ajax({
-        type: 'post',
-        url: 'supprimerArticle.php',
-        data: {
-          table:typeCat,
-          nom:name
-        },
-        success: function (response) {
-          alert('element supprimé');          
-          GetData();
-        }
-    });
-    return false;
+function ajouterArticle() {
+  let name = document.getElementById('nouveau_nom').value;
+  let description = document.getElementById('nouvelle_description').value;
+  let photo = document.getElementById('nouvelle_photo').value;
+  let typeCat = document.getElementById("type_principal").value;
+  let formData = new FormData();
+  formData.append('nom', name);
+  formData.append('description', description);
+  formData.append('table', typeCat);
+  formData.append('photo', document.getElementById('nouvelle_photo').files[0]);
+  $.ajax({
+    type: 'post',
+    url: 'ajouterArticle.php',
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      alert('L\'article '.concat(response, ' a bien été ajouté !'));
+      GetData();
+    }
+  });
+  return false;
 }
 
-function ajouterArticle()
-{
-    let name = document.getElementById('nouveau_nom').value;
-    let description = document.getElementById('nouvelle_description').value;
-    let photo = document.getElementById('nouvelle_photo').value;
-    let typeCat = document.getElementById("type_principal").value;
-    let formData = new FormData();
-    formData.append('nom', name);
-    formData.append('description', description);
-    formData.append('table', typeCat);
-    formData.append('photo', document.getElementById('nouvelle_photo').files[0]);
-    $.ajax({
-        type: 'post',
-        url: 'ajouterArticle.php',
-        data: formData,
-        processData: false,
-        contentType : false,
-        success: function (response) {
-          alert('L\'article '.concat(response,' a bien été ajouté !'));
-          GetData();
-        }
-    });
-    return false;
-}
-
-function confirmerModificationArticle(e){
+function confirmerModificationArticle(e) {
   let typeCat = document.getElementById("type_principal").value;
   let name = document.getElementById('new_name');
   let oldName = name.placeholder;
-  if (name.value == ''){
+  if (name.value == '') {
     name = oldName;
-  }else{
+  } else {
     name = name.value;
   }
   let description = document.getElementById('new_desc').value;
   let formData = new FormData();
-  if (document.getElementById('new_photo').value == ''){
+  if (document.getElementById('new_photo').value == '') {
     formData.append('photo', e.parentElement.parentElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.title);
   } else {
     formData.append('photo', document.getElementById('new_photo').files[0]);
   }
-  formData.append('table', typeCat);  
+  formData.append('table', typeCat);
   formData.append('nom', name);
   formData.append('oldName', oldName);
   formData.append('description', description);
@@ -91,16 +88,16 @@ function confirmerModificationArticle(e){
     url: 'modifierArticle.php',
     data: formData,
     processData: false,
-    contentType : false,
+    contentType: false,
     success: function (response) {
-      alert('L\'article '.concat(response,' a bien été modifié !'));
+      alert('L\'article '.concat(response, ' a bien été modifié !'));
       GetData();
     }
-});
-return false;
+  });
+  return false;
 }
 
-function modifierArticle(e){
+function modifierArticle(e) {
   let title = e.parentElement.parentElement.firstElementChild.nextElementSibling.firstElementChild;
   let oldTitle = title.cloneNode([true]);
   let image = title.nextElementSibling;
@@ -114,7 +111,7 @@ function modifierArticle(e){
   test.name = "new_photo";
   test.id = "new_photo";
   let centre = document.createElement("div");
-  centre.setAttribute('class','d-flex justify-content-center align-items-center h-100');
+  centre.setAttribute('class', 'd-flex justify-content-center align-items-center h-100');
   centre.appendChild(test);
   image.appendChild(centre);
   let oldDescriptionValue = description.innerHTML;
@@ -123,8 +120,8 @@ function modifierArticle(e){
   let oldfooter = footer.cloneNode([true]);
   let annuler = document.createElement("button");
   annuler.type = 'button';
-  annuler.setAttribute('class', 'btn btn-secondary btn-lg');  
-  annuler.onclick = function() {
+  annuler.setAttribute('class', 'btn btn-secondary btn-lg');
+  annuler.onclick = function () {
     title.replaceWith(oldTitle);
     image.replaceWith(oldImage);
     description.replaceWith(oldDescription);
@@ -134,5 +131,5 @@ function modifierArticle(e){
   annuler = footer.insertBefore(annuler, e);
   e.innerHTML = 'Confirmer';
   e.setAttribute('class', 'btn btn-primary btn-lg');
-  e.onclick = function() {confirmerModificationArticle(e)};
+  e.onclick = function () { confirmerModificationArticle(e) };
 }
