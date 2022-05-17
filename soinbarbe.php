4720237ec233274057c307a869caf4e23991f162
Sergio Gonzalez <s.gonzalez@codeur.online>
2022-05-17 14:52:52 +0200

    soinbarbe terminé

--- soinbarbe.php
+++ soinbarbe.php
@@ -16,9 +16,7 @@
 </head>
 
 <body>
-    <header>
         <?php include_once ('header.php');?>
-    </header>
 
     <main>
 
@@ -35,15 +33,15 @@
             <p class="blanc citation">
                 Nous vous proposons diférents services dans nos salons répartis à travers l'Europe.
             </p>
+            <p id="para1"> De la crème, du baume en passant par des blaireaux<br> du shampoings et bien d'autre
+                cosmetique & accesoires!</p>
 
         </section>
 
         <section class="section3">
-            <p id="para1"> De la crème, du baume en passant par des blaireaux<br> du shampoings et bien d'autre
-                cosmetique & accesoires!</p>
+            
             <img src="img/deco2.svg" alt="decorationn en carré coloré" id="deco2svg">
         </section>
-        <br>
 
 
         <section class="Baume">
@@ -55,69 +53,72 @@
                 <div class="ligneGauche">
                 </div>
 
-                <!--  <button type="button">
-                <img src="img/fleche.png" alt="fleche noir" id="fleche1">
-            </button>-->
-
                 <div class="carteBaume">
                     <figure>
-                        <img src="img/coiffant.jpg" alt="baume coiffant barbe" id="coiffant">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/coiffant.jpg" alt="baume coiffant barbe" id="coiffant">
+                        </div>
                         <figcaption>
                             Baume "Tech Art" <br> Fix Design L'Oréal
                         </figcaption>
                     </figure>
 
                     <figure>
-                        <img src="img/coiffant1.png" alt="baume coiffant barbe">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/coiffant1.png" alt="baume coiffant barbe">
+                        </div>
                         <figcaption>
                             Baume "GoldenBeards" <br> HEARD BALM
                         </figcaption>
                     </figure>
 
                     <figure>
-                        <img src="img/coiffant2.png" alt="baume coiffant barbe" id="coiffant2">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/coiffant2.png" alt="baume coiffant barbe" id="coiffant2">
+                        </div>
                         <figcaption>
                             Baume "Poraso" <br> Baume pour la barbe
                         </figcaption>
                     </figure>
                 </div>
-
-                <input type="button">
                 <img src="img/fleche.png" alt="fleche noir" id="fleche2">
                 </input>
             </div>
         </section>
-        <br>
 
         <section class="shampoing">
 
-
-            <div class="conteneur1">
-                <p id="para3">
+            
+            <div class="conteneur" id='conteneur2'>
+                <p id="para2">
                     Shampoings
                 </p>
-                <!-- <button>
-            <img src="img/fleche.png" alt="fleche noir" id="fleche3">
-        </button> -->
+                <img src="img/fleche.png" alt="fleche noir" id="fleche3">
 
-                <div class="carteshamp">
+                <div class="carteBaume">
 
                     <figure>
-                        <img src="img/shampoing.jpg" alt="shampoing noir" class="imgshamp" id="shampnoir">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/shampoing.jpg" alt="shampoing noir" class="imgshamp" id="shampnoir">
+                        </div>
                         <figcaption>
                             Shampoing <br> "The barber Company" 250 ml
                         </figcaption>
                     </figure>
 
                     <figure>
-                        <img src="img/shampoing1.png" alt="shampoing noir" class="imgshamp">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/shampoing1.png" alt="shampoing noir" class="imgshamp">
+                        </div>
                         <figcaption>
                             Shampoing Cheveux Sec au Miel<br>"Osma Tradition Since 1957" 200 ml
                         </figcaption>
                     </figure>
 
                     <figure>
-                        <img src="img/shampoing2.png" alt="shampoing noir" class="imgshamp" id="shampoing2">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/shampoing2.png" alt="shampoing noir" class="imgshamp" id="shampoing2">
+                        </div>
                         <figcaption>
                             Shampoing Grey de la marque espagnole Beardburys. <br> Spécialement conçu pour les cheveux blancs et gris
                         </figcaption>
@@ -125,21 +126,16 @@
 
                 </div>
 
-                <button>
-                    <img src="img/fleche.png" alt="fleche noir" id="fleche4">
-                </button>
-
-                <div class="ligneDroite">
+                
+                <div class="ligneGauche">
                 </div>
-
+    
             </div>
         </section>
 
 
 
         <section class="huiles">
-
-            <img src="img/deco2.svg" alt="image de décoration orange" id="imgDeco2SVG">
 
             <p id="para4">
                 Huiles
@@ -148,45 +144,47 @@
             <div class="conteneur2">
                 <div class="cartehuile">
 
-                    <!--    <button>
-            <img src="img/fleche.png" alt="fleche noir" id="fleche5">
-                  </button> -->
-
                     <figure>
-                        <img src="img/huile.PNG" alt="huiles" class="imghuile">
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/huile.PNG" alt="huiles" class="imghuile">
+                        </div>    
                         <figcaption>Huile à barbe KEUNE<br> traitement au satin 95ml</figcaption>
                     </figure>
 
                     <figure>
-                        <img src="img/huile2.PNG" alt="huiles" class="imghuile">
-                        <figcaption>Huile à barbe AZBANE<br> Beard Oil 30ml</figcaption>
+                        <div class='img-centre'>
+                            <img class="petite-image" src="img/huile2.PNG" alt="huiles" class="imghuile">
+                        </div>
+                            <figcaption>Huile à barbe AZBANE<br> Beard Oil 30ml</figcaption>
                     </figure>
-
-
-                    <button>
-                        <img src="img/fleche.png" alt="fleche noir" id="fleche6">
-                    </button>
+                    <img src="img/fleche.png" alt="fleche noir" id="fleche4">
                 </div>
-            </div>
+                
         </section>
-        <BR>
 
         <section class="cireBarbe">
-            <img src="/img/bois.jpg" alt="planche en bois" id="imgBois">
+            <img src="img/deco2.svg" alt="image de décoration orange" id="imgDeco2SVG">
+
             <p id="para5">
                 Cire à Barbe
             </p>
+            <img src="/img/bois.jpg" alt="planche en bois" id="imgBois">
+            
 
             <div class="carteCire">
                 <figure>
-                    <img src="/img/cire1.PNG" alt="cire 1" class="imgCire">
+                    <div class='img-centre'>
+                        <img class="petite-image" src="/img/cire1.PNG" alt="cire 1" class="imgCire">
+                    </div>
                     <figcaption>
                         Cire Cheveux/Barbe<br> "Le père Lucien" 15g
                     </figcaption>
                 </figure>
 
                 <figure>
-                    <img src="/img/cire2.PNG" alt="cire 1" class="imgCire">
+                    <div class='img-centre'>
+                        <img class="petite-image" src="/img/cire2.PNG" alt="cire 1" class="imgCire">
+                    </div>    
                     <figcaption>
                         Moustache WAX <br> "Dr.K's 100% Natural Handmade Stylin" 15g
                     </figcaption>
@@ -194,7 +192,7 @@
             </div>
         </section>
     </main>
-    <!-- <?php include_once ('footer.php');?> -->
+    <?php include_once ('footer.php');?>
 </body>
 
 </html>
