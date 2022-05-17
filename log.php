a9fc7e10a1b0f7dff0ce709b6a2252f49064b2f2
Enola Tutellier <enola.tutellier@hotmail.com>
2022-05-17 14:53:22 +0200

    php

--- log.php
+++ log.php
@@ -1,49 +1,30 @@
 <?php
+//echo "Bienvenu sur la page de connexion";
 
-if(isset($_POST['mail']))
-{
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
+if(isset($_POST['mail']) && isset($_POST['pass'])) { //On verifie si l'utilisateur à rentré ses informations
+  //Mettre l'email et le mdp dans des variables
+  $mail = $_POST['mail'];
+  $pass = $_POST['pass'];
+   $server = "localhost";
+   $dbname = "barber";
+   $user = "root";
+   $pass = "root";
+   $db = new PDO("mysql:host=$server;dbname=barber", $user, $pass);
+   //requete pour selectionner l'utilisateur qui a pour mail et mot de passe les identifiant qui ont été entrée dans la bdd
+   $sql = "SELECT * FROM users where Mail = '$mail' ";
+   $result = $db->prepare($sql);
+   $result ->execute();
+   $adresses = $result-> fetchAll(PDO::FETCH_ASSOC);
+   
+   $num_ligne = count ($adresses) ; // nombre de ligne ayant rapport à la requete sql 
+   if($num_ligne >= 1)
+   {
+      header("Location:index.php") ; // si le nombre de ligne est > 0 on sera dirigé a la page index
+   }else{ //sinon
+      echo "Adresse mail ou mot de passe incorrectes !" ;
 
-    try{
+   }
 
-        $conn = new PDO("mysql:host=$servername;dbname=exercices_sql", $username, $password);
-        
-        //On définit le mode d'erreur de PDO sur Exception
-        
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        echo 'Connexion réussie';
-        
-        }
-        
-        /*On capture les exceptions si une exception est lancée et on affiche
-        
-        *les informations relatives à celle-ci*/
-        
-        catch(PDOException $e){
-        
-        echo "Erreur : " . $e->getMessage();
-        }
-
-
-    $sql = "SELECT * FROM user where email = '$email' ";
-    $result = $db->prepare($sql);
-    $result-> execute();
-
-        if ($result ->rowCount() > 0)
-
-    {
-        echo "Connexion reussi";
-    }
-        else
-    {
-        $pass = password_hash($pass, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO user (email, password) VALUES ('$email' , '$pass') ";
-        $req = $db->prepare($sql);
-        $req->execute();
-        
-    }
 }
 ?> 
 
